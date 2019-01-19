ec11cdb6860d9f0045e27df2f8e368eff4ae9fc2
Unknown <ltyue@ualberta.ca>
2019-01-19 14:09:27 -0700

    gets commits from user/reponame in function parse_repo

--- js_test/json_parse.js
+++ js_test/json_parse.js
@@ -7,7 +7,7 @@ var jobj;
 var sample_git = "https://api.github.com/repos/learningequality/ka-lite/commits";
 
 function http_get(arg) {
-var client = new XMLHttpRequest();
+	var client = new XMLHttpRequest();
   client.open('GET', arg);
   client.onreadystatechange = function() {
     // console.log(client.responseText);
@@ -22,3 +22,9 @@ function parse_json(file) {
   jobj = JSON.parse(file);
 }
 
+function parse_repo(arg) {
+
+	var search = "https://api.github.com/repos/" + arg + "/commits";
+	console.log(search);
+	http_get(search);
+}
