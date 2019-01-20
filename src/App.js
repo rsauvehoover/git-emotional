93c03ee176335f5d78f5e544cc7622dfc47c3832
Kevin de Haan <kbehaan@gmail.com>
2019-01-20 05:49:28 -0700

    fade-in fade-out transitions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './css/App.css';
 import SubmitPage from './screens/submitPage.js';
 import StatPage from './screens/statPage.js';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
@@ -9,11 +10,22 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Switch>
-          <Route path="/" component={SubmitPage} exact={true} />
-          <Route path="/stat/" component={StatPage} exact={true} />
-          <Redirect to="/" />
-        </Switch>
+        <Route render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition
+              key={location.key}
+              timeout={300}
+              classNames='fade'
+            >
+              <Switch location={location}>
+                <Route path="/" component={SubmitPage} exact={true} />
+                <Route path="/stat/" component={StatPage} exact={true} />
+                <Redirect to="/" />
+                <Route render={() => <div>Not Found</div>} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}/>
       </Router>
     );
   }
