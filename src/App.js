d94ca9f7afa2744d829f47432224d127792c960f
Kevin de Haan <kbehaan@gmail.com>
2019-01-19 19:00:59 -0700

    found an alternative to actually doing anything correctly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import './css/App.css';
-import './sentimood.js'
-import './client.js';
 import SubmitPage from './screens/submitPage';
 
 class App extends Component {
