5def9615654679b0bd377d8a731f8e284e146ce0
Kevin de Haan <kbehaan@gmail.com>
2019-01-19 22:11:50 -0700

    moved sentiment handling test

--- src/screens/statPage.js
+++ src/screens/statPage.js
@@ -1,6 +1,12 @@
 import React, { Component }  from 'react';
+import Sentimood from '../sentiment/sentimood.js';
 
 export default class StatPage extends Component {
+  componentDidMount() {
+    var sentimood = new Sentimood();
+    // Example of a function using sentimood
+    console.log(sentimood.analyze('fuck this shit'));
+  }
   render() {
     return (
       <div>Filler stuff</div>
