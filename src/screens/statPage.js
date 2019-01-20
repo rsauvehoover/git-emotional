caa7d9e410fc9577c494ae26e811028a9777d2be
Kevin de Haan <kbehaan@gmail.com>
2019-01-19 23:21:04 -0700

    line chart example

--- src/screens/statPage.js
+++ src/screens/statPage.js
@@ -1,15 +1,24 @@
 import React, { Component }  from 'react';
 import Sentimood from '../sentiment/sentimood.js';
+import LinePlot from './line';
+
 
 export default class StatPage extends Component {
   componentDidMount() {
     var sentimood = new Sentimood();
     // Example of a function using sentimood
     console.log(sentimood.analyze('this don\'t good'));
+
   }
   render() {
     return (
-      <div>Filler stuff</div>
+      <div>
+        <div>Filler stuff</div>
+        <div>
+          <LinePlot />
+        </div>
+      </div>
+      
     );
   }
 }
